24d318f508e555ae5bc4f64e553ebcca7a4216c6
shee333 <sheeshrong@outlook.com>
2026-03-31 20:13:14 +0800

    chore: 更新组件导出列表
    - 添加 NodeConfigPanel 到导出列表

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,5 +4,6 @@ export { ComponentPalette } from './ComponentPalette';
 export { CodeEditor } from './CodeEditor';
 export { WorkflowIDE } from './WorkflowIDE';
 export { ExecutionPanel } from './ExecutionPanel';
+export { NodeConfigPanel } from './NodeConfigPanel';
 export type { ExecutionStatus, ExecutionLog } from './ExecutionPanel';
 export * from './types';
